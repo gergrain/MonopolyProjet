6fdf0e5f9f70ffa0aed66239e48c30a010297703
JonathanLathiere <jonathan.lathiere@etu.unilim.fr>
2015-02-11 19:26:23 +0100

    Board monopoly

--- board.js
+++ board.js
@@ -1,19 +1,35 @@
 $(document).ready(genererBoard);
 function genererBoard(){
     var table = $('<table></table>');
+    table.attr('width', '400px');
+    table.attr('height', "400px");
     table.attr('border', '1');
     for (var ligne = 0; ligne < 11; ligne++){
-       var tr = $('<tr></tr>');
+        var tr = $('<tr></tr>');
         table.append(tr);
-        for (var colonne=0; colonne < 11; colonne++){
-            var td = $('<td></td>');
-            td.attr('id', ligne+","+colonne);
-            td.text(" ");
-            tr.append(td);
+        if(ligne == 0 || ligne == 10){
+            for (var colonne=0; colonne < 11; colonne++){
+                var td = $('<td></td>');
+                td.attr('id', ligne+","+colonne);
+                td.text(" ");
+                tr.append(td);
+            }
+        }else{
+            for (var colonne=0; colonne < 3; colonne++){
+                if(colonne == 1){
+                    var td = $('<td></td>');
+                    td.attr('id', ligne+","+colonne);
+                    td.attr('colspan', '9');
+                    td.text(" ");
+                    tr.append(td);
+                }else{
+                    var td = $('<td></td>');
+                    td.attr('id', ligne+","+colonne);
+                    td.text(" ");
+                    tr.append(td);
+                }                                
+            }
         }
-    }
-    for (var i=1; i < 11; i++){
-        
     }
     $('#tableau').append(table);
 }
