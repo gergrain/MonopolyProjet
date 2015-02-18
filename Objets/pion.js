a6ef30492ecfcf152a41240f19ed8297e1e8b4ed
Corentin Ambroise <corentin.ambroise@etu.unilim.fr>
2015-02-18 14:11:11 +0100

    Pion

--- Objets/pion.js
+++ Objets/pion.js
@@ -4,5 +4,18 @@ function Pion(color, joueur,img) {
 	this.pion = img;
 
 	this.deplacer = function (des){
-		this.position = this.position + des;
+		while(des != 0){
+			if (this.position =40){
+				this.position = 0
+			}else{
+				this.position ++;
+			}
+			des--;
+		}
 	}
+
+	this.goTo = function (position){
+		this.position = position
+	}
+
+}
