1b183f3cdd7f31b0c9fb47bcb29e4dade423fce4
gergrain <akpa.germain@gmail.com>
2015-02-18 14:19:03 +0100

    js

---
+++ node/public/js/cartes.js
@@ -0,0 +1,38 @@
+
+	var cartesChance = [{id:"1",categorie:"1",texte:"Allez au départ"},
+					{id:"2",categorie:"1",texte:"Allez en prison"},
+					{id:"3",categorie:"1",texte:"Allez Bld de la Villette"},
+					{id:"4",categorie:"1",texte:"Amende pour ivresse -2000€"},
+					{id:"5",categorie:"1",texte:"Vous avez gagné aux mot-croisés"},
+					{id:"6",categorie:"1",texte:"Allez Rue Henri Martin"},
+					{id:"7",categorie:"1",texte:"Allez Gare de Lyon"},
+					{id:"8",categorie:"1",texte:"Amende pour excès de vitesse -1500€"},
+					{id:"9",categorie:"1",texte:"Payez les frais de scolarité -15000€"},
+					{id:"10",categorie:"1",texte:"Reculez de 3 cases"},
+					{id:"11",categorie:"1",texte:"Réparations dans votre maison(-2500€/Maison -10000€/Hotel)"},
+					{id:"12",categorie:"1",texte:"Votre prêt vous rapporte 15000€"},
+					{id:"13",categorie:"1",texte:"Libéré de prison"},
+					{id:"14",categorie:"1",texte:"RDV Rue de la Paix"},
+					{id:"15",categorie:"1",texte:"Voirie 4000€/maison 11500€/hotel "},
+					{id:"16",categorie:"1",texte:"La banque vous verse 5000€"}
+                       ];
+					
+
+	var cartesCommunautee = [{id:"1",categorie:"2",texte:"Vous gagnez 10000€ en jouant au casino"},
+					{id:"2",categorie:"2",texte:"Votre pauvre oncle Joey est encore en prison. Payer pour lui un caution de $5000€"},
+					{id:"3",categorie:"2",texte:"Votre groupe de rock est arrivé premier lors du concours 'Battle of the Bands' Recevez 1000€"},
+					{id:"4",categorie:"2",texte:"Vous célébrez vos fiançailles : chaque joueur doit voud donner 1000€"},
+					{id:"5",categorie:"2",texte:"Erreur de la banque de Hill Valley en votre faveur. Recevez 20000€"},
+					{id:"6",categorie:"2",texte:"Recevez 2000€ pour votre remarquable performance de 'Féérie Dansante des Sirènes"},
+					{id:"7",categorie:"2",texte:"Recevez 10000€ pour la sortie de votre 1er disque "},
+					{id:"8",categorie:"2",texte:"Recevez 5000€ de récompense pour l'arrestation de Buford Molosse Tannen"},
+					{id:"9",categorie:"2",texte:"Placez vous sur la case 'Départ'"},
+					{id:"10",categorie:"2",texte:"Vous faites convertir votre véhicule en aéroglisseur. Payer 10000€"},
+					{id:"11",categorie:"2",texte:"Vous actions TEXACO vous rapportent la somme de 2500€"},
+					{id:"12",categorie:"2",texte:"Retourner à la gare de Hill Valey"},
+					{id:"13",categorie:"2",texte:"Payez votre cure de rajeunissement à la clinique 5000€"},
+					{id:"14",categorie:"2",texte:"Vous êtes libéeé de retenue. Cette carte peut être conservée jusqu'à ce qu'elle soit utilisée ou vendue"},
+					{id:"15",categorie:"2",texte:"Allez en retenue. Avancez tout droit en retenue. Ne passez pas par la case 'Départ'. Ne recevez pas 20000€"},
+					{id:"16",categorie:"2",texte:"Biff vous fait chanter : Payez 1000€ ou bien tirez une carte 'CHANCE'"}
+                            ];
+					
