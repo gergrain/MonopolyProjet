624b3520e07eb9dca9d28b3f1540314150a6efeb
Corentin Ambroise <corentin.ambroise@etu.unilim.fr>
2015-03-04 08:47:19 +0100

    des

--- node/public/js/des.js
+++ node/public/js/des.js
@@ -5,12 +5,16 @@ function getNumber() {
         var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // la fonction magique
         return randomnumber;
     }  
+window.onload=function(){
+    $("body").append("<div id='de1' onclick=\"lancer()\" style='position:absolute; visibility:visible; left:350px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_1.png'/></div>");
+        $("body").append("<div id='de2' onclick=\"lancer()\" style='position:absolute; visibility:visible; left:300px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_1.png'/></div>");
 
+}
 function lancer(){
         de=Math.ceil(6*Math.random())
         $("#de1").remove();
         $("#de2").remove();
-        $("body").append("<div id='de1' style='position:absolute; visibility:visible; left:350px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_"+de+".png'/></div>");
-        $("body").append("<div id='de2' style='position:absolute; visibility:visible; left:300px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_"+getNumber()+".png'/></div>");
+        $("body").append("<div id='de1' onclick=\"lancer()\" style='position:absolute; visibility:visible; left:350px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_"+de+".png'/></div>");
+        $("body").append("<div id='de2' onclick=\"lancer()\" style='position:absolute; visibility:visible; left:300px; top:100px; z-index:999;'><img class='img_zoom_image' src='/images/de_"+getNumber()+".png'/></div>");
 
  }
