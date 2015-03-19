bc3d8c03c8e077c7d34ab14a732db1d6e15e6501
gergrain <akpa.germain@gmail.com>
2015-03-19 15:29:17 +0100

    odjnveqonv

--- node/app.js
+++ node/app.js
@@ -11,7 +11,7 @@ var express         = require('express'),app = express(),
 
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname +'/index.html');
+  res.sendFile(__dirname +'/index.html');
 });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', 6900);
